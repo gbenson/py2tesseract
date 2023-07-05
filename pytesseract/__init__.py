41489c028dae624410d99460b6b5529f3509146b
Gary Benson <gary.benson@appcheck-ng.com>
2023-07-05 11:36:07 +0100

    Add from __future__ imports

--- pytesseract/__init__.py
+++ pytesseract/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 # flake8: noqa: F401
 from .pytesseract import ALTONotSupported
 from .pytesseract import get_languages
